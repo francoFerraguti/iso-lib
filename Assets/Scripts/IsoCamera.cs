2b654fb05924a2befa7424e27f893f0fdd2808d4
Franco Ferra <Franco Ferra>
2018-12-18 12:00:56 -0300

    IsoCamera

---
+++ Assets/Scripts/IsoCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class IsoCamera
+{
+    public static float zoomMultiplier = 2.0f;
+
+    public static void Init()
+    {
+        Camera.main.transform.position = new Vector3(0, -0.5f, 0);
+        Camera.main.transform.eulerAngles = new Vector3(30, -45, 0);
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 5;
+        Camera.main.nearClipPlane = -10;
+        Camera.main.farClipPlane = 100;
+    }
+
+    public static void Zoom(float amount)
+    {
+        Camera.main.orthographicSize += amount * zoomMultiplier;
+    }
+}
---
+++ Assets/Scripts/TestingHelperScene1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingHelperScene1 : MonoBehaviour
+{
+    void Awake()
+    {
+        IsoCamera.Init();
+        IsoCamera.zoomMultiplier = 4;
+    }
+
+    void Update()
+    {
+        IsoCamera.Zoom(-Input.GetAxis("Mouse ScrollWheel"));
+    }
+}
